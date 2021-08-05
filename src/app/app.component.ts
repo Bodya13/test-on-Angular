498f87c4b037f1cd0de83f08353901b59c1a95cb
Bohdana <47737949+Bodya13@users.noreply.github.com>
2021-08-05 08:42:36 +0300

    finished test task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-on-Angular';
+  text = 'Press the button below to see a random string'
+  statusBtn = 'block'
+
+  randomString() {
+    let randomString = '';
+    const symbols = '1234567890qwertyuiopasdfghjklzxcvbnm';
+    const symbolCounting = 5;
+
+    for( let i = 0; i < symbolCounting; i++ ) {
+      const index = Math.floor (Math.random() * symbols.length);
+      randomString += symbols[index];
+    }
+
+    if ( randomString.includes('0')) {
+      this.randomString()
+    } else this.text = randomString;
+  }
+
+  randomStringLaunch () {
+    timer(0, 3000).pipe().subscribe(() => {
+      this.randomString()
+    })
+
+    this.statusBtn = 'none'
+  }
+
+  isPalindrome () {
+    const reverseText = this.text.split('').reverse().join('')
+    return this.text === reverseText 
+  }
+
+  isOnlyNumbers() {
+    let isNumber = true
+
+    for ( let i = 0; i < this.text.length; i++ ) {
+      if (!Number(this.text[i])) {
+        isNumber = false
+        break
+      }
+    }
+    
+    return isNumber
+  }
 }
